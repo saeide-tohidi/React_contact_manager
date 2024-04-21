86d8f162d1ec5fade710f07b137860d3f3106100
tohidi <s.tohidi22@gmail.com>
2024-04-21 17:42:47 +0330

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h3>Caontaact manager application</h3>
+      <h3>Caontact manager application</h3>
+      <span className='fa fa-user'></span>
+      <button type="button" class="btn btn-primary">button</button>
+
     </div>
   );
 }
