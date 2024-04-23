12e616249f6d98314140d27befebcdc0a8692a24
tohidi <s.tohidi22@gmail.com>
2024-04-23 09:31:15 +0330

    navbar & search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
+import Navbar from "./components/Navbar";
+
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <h3>Caontact manager application</h3>
-      <span className='fa fa-user'></span>
-      <button type="button" class="btn btn-primary">button</button>
-
+      <Navbar />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/contact/SearchContact.jsx
@@ -0,0 +1,26 @@
+import { PURPLE, BACKGROUND, COMMENT } from '../../helpers/colors'
+
+const SearchContact = () => {
+  return (
+    <div className="input-group mx-2 w-75" dir="ltr">
+      <span
+        className="input-group-text"
+        id="basic-addon1"
+        style={{ backgroundColor: PURPLE }}
+      >
+        <i className="fas fa-search" />
+      </span>
+      <input
+        dir="rtl"
+        type="text"
+        style={{ backgroundColor: COMMENT, borderColor: PURPLE }}
+        className="form-control"
+        placeholder="جستجوی مخاطب"
+        aria-label="Search"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+  );
+};
+
+export default SearchContact;
