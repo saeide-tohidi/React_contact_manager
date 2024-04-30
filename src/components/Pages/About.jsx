a078eb5655892a7062a93115edbab1a46eede7fb
tohidi <s.tohidi22@gmail.com>
2024-05-01 00:11:02 +0330

    react lifeCycle

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,11 +1,51 @@
+import { Component } from "react";
 import { ORANGE } from "../../helpers/colors";
 
-export default function About() {
-    return (
-        <div className="text-center py-5 ">
-            <p className="h3" style={{ color: ORANGE }}>درباره‌ی ما</p>
-        </div>
-    )
 
+
+class About extends Component {
+
+    constructor() {
+        super();
+        console.log('About... constructor');
+        this.state = { date: new Date(), showClock: true, color: false }
+    }
+
+    static getDerivedStateFormProps(props, state) {
+        console.log('About... getDerivedStateFormProps');
+        return state
+
+    }
+
+    componentDidMount() {
+        console.log('About... componentDidMount');
+        this.timer = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        console.log('About... componentWillUnmount');
+        clearInterval(this.timer);
+
+    }
+
+    tick() {
+
+        this.setState({ date: new Date() })
+    }
+
+    render() {
+
+        return (
+            <div className="text-center py-5 ">
+                <p className="h3" style={{ color: ORANGE }}>درباره‌ی ما</p>
+                <br />
+                <p className="h3" style={{ color: ORANGE }}>ساعت در حال حاضر برابر است با : {this.state.date.toLocaleTimeString()}</p>
+            </div>
+        )
+
+    }
 }
+
+
+export default About;
 
